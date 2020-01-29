3901fb2342d25e809e3fdf5f867a5f22a8c41c79
Carlos Hernández López <adictivitic@outlook.com>
2020-01-29 14:35:21 -0600

    Reestructura inicial

---
+++ Sales.Domain/Models/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace Sales.Domain.Models
+{
+    /*Clase que utilizara Entity Framework, por lo tanto,
+    hereda de la clase DbContext*/
+    public class DataContext: DbContext
+    {
+        /*Llamamos al constructor de la clase DbContext y le pasamos
+         como parametro nuestra cadena de conexion (esta la podemos encontrar
+         en el archivo config de el proyecto backend o el proyecto API).*/
+        public DataContext(): base("DefaultConnection")
+        {
+
+        }
+
+        /*CADA VEZ QUE SE INSTANCIE ESTA CLASE CREARA UNA CONEXION A LA BASE DE DATOS*/
+    }
+}
